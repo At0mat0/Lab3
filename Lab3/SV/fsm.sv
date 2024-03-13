e20d7caa2e23cb0a2a9b90130cf778a2178ddae7
At0mat0 <evan.cole@okstate.edu>
2024-03-13 16:11:54 -0500

    Update fsm.sv
    Testing left and right

--- Lab3/SV/fsm.sv
+++ Lab3/SV/fsm.sv
@@ -1,12 +1,14 @@
-module FSM (clk, reset, a, y);
+module FSM (clk, reset, Left, Right, y, all);
 
    input logic  clk;
    input logic  reset;
-   input logic 	a;
+   input logic 	Left;
+   input logic 	right;
+   input logic 	All;
    
    output logic y;
 
-   typedef enum 	logic [1:0] {S0, S1, S2} statetype;
+   typedef enum 	logic [1:0] {S0, S1, S2, S3, R1, R2 ,R3, A1} statetype;
    statetype state, nextstate;
    
    // state register
@@ -18,22 +20,48 @@ module FSM (clk, reset, a, y);
    always_comb
      case (state)
        S0: begin
-	  y <= 1'b0;	  
-	  if (a) nextstate <= S0;
-	  else   nextstate <= S1;
-       end
-       S1: begin
-	  y <= 1'b0;	  	  
-	  if (a) nextstate <= S2;
-	  else   nextstate <= S1;
-       end
-       S2: begin
-	  y <= 1'b1;	  	  
-	  if (a) nextstate <= S2;
+	  y <= 6'b000000;	  
+	  if (left) nextstate <= S1;
+    if (Right) nextstate <= R1;
 	  else   nextstate <= S0;
        end
+  S1: begin
+	  y <= 6'b001000;	  	  
+	  if (left) nextstate <= S2;
+	  else   nextstate <= S1;
+       end
+  S2: begin
+	  y <= 6'b011000;	  	  
+	  if (left) nextstate <= S3;
+	  else   nextstate <= S0;
+       end
+   S3: begin
+	  y <= 6'b111000;	  	  
+	  if (left) nextstate <= S0;
+	  else   nextstate <= S0;
+       end
+R1: begin
+	  y <= 6'b000001;	  	  
+	  if (right) nextstate <= R1;
+	  else   nextstate <= S0;
+       end
+R2: begin
+	  y <= 6'b000011;	  	  
+	  if (right) nextstate <= R2;
+	  else   nextstate <= S0;
+       end
+R3: begin
+	  y <= 6'b000111;	  	  
+	  if (right) nextstate <= R3;
+	  else   nextstate <= S0;
+       end
+ALL: begin
+	  y <= 6'b111111;	  	  
+	  if (a) nextstate <= ALL;
+	  else   nextstate <= S0;
+       end       
        default: begin
-	  y <= 1'b0;	  	  
+	  y <= 6'b000000;	  	  
 	  nextstate <= S0;
        end
      endcase
